599036759f94f90e43276d7e0ac164978a693ade
transistor <golin.setup@gmail.com>
2018-02-19 16:17:06 -0300

    primero commit

---
+++ src/com/setupservice/model/Facultad.java
@@ -0,0 +1,48 @@
+package com.setupservice.model;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Facultad
+ *
+ */
+//@Entity
+
+public class Facultad implements Serializable {
+
+	   
+	@Id
+	private Integer id;
+	private String descripcion;
+	private String sigla;
+	private static final long serialVersionUID = 1L;
+
+	public Facultad() {
+		super();
+	}   
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}   
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}   
+	public String getSigla() {
+		return this.sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+   
+}
